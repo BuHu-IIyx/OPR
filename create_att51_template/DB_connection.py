0b48641b715ec20446abd62b2e5e6246ff5860ff
BuHu-IIyx <Buhu.iiyxs@gmail.com>
2022-10-01 11:07:16 +0300

    Refactoring part 2

--- create_att51_template/DB_connection.py
+++ create_att51_template/DB_connection.py
@@ -1,4 +1,6 @@
 import json
+import os
+import shutil
 from datetime import datetime
 import pyodbc
 
@@ -6,6 +8,7 @@ import pyodbc
 class DBConnector:
     def __init__(self, conn_str, sql_dict_json_file):
         self.res_dict = {}
+        self.db_path = conn_str
         db_conn_str = f'DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}}; DBQ={conn_str}\\ARMv51.MDB;'
         try:
             connect = pyodbc.connect(db_conn_str)
@@ -16,15 +19,18 @@ class DBConnector:
         with open(sql_dict_json_file, 'r', encoding="utf-8-sig") as file:
             self.sql_dict = json.load(file)
 
-    def create_template_json(self, json_address):
-        rms = self.get_rm_from_db('template')
+    def create_template_json(self, template_name):
+        rms = self.get_rm_from_db(template_name)
         for rm in rms:
-            self.res_dict[rm[1]] = {}
-            for key in self.sql_dict.keys():
-                self.read_table(key, rm[0], rm[1])
-        self.save_res_dict(json_address)
+            self.copy_DB_files(rm[1], rm[2], template_name)
+            if rm[1] not in self.res_dict.keys():
+                self.res_dict[rm[1]] = {}
+                for key in self.sql_dict.keys():
+                    self.read_table(key, rm[0], rm[1])
+        self.save_res_dict(template_name)
 
-    def save_res_dict(self, json_address):
+    def save_res_dict(self, template_name):
+        json_address = f'C:\\Users\\buhu_\\PycharmProjects\\OPR\\output\\Templates\\{template_name}\\template.json'
         try:
             with open(json_address, 'w', encoding="utf-8-sig") as file:
                 json.dump(self.res_dict, file, ensure_ascii=False, indent=4)
@@ -46,7 +52,7 @@ class DBConnector:
                 self.res_dict[rm_name][table].append(res_list)
 
     def get_rm_from_db(self, org_name):
-        sql = 'SELECT rm.id, rm.caption FROM (struct_rm rm INNER JOIN struct_ceh ceh ON rm.ceh_id = ceh.id) ' \
+        sql = 'SELECT rm.id, rm.caption, rm.mguid FROM (struct_rm rm INNER JOIN struct_ceh ceh ON rm.ceh_id = ceh.id) ' \
               'INNER JOIN struct_org org ON ceh.org_id = org.id WHERE org.caption = ?'
         res = self.cursor.execute(sql, org_name).fetchall()
         return res
@@ -58,3 +64,19 @@ class DBConnector:
 
         except pyodbc.Error as e:
             print("Error in Connection", e)
+
+    def copy_DB_files(self, rm_name, path_mguid, res_folder):
+        dist = self.db_path + '\\ARMv51_files\\' + path_mguid
+        p_dir = f'C:\\Users\\buhu_\\PycharmProjects\\OPR\\output\\Templates\\{res_folder}\\{rm_name}'
+        res_path = self.check_folder(p_dir)
+        try:
+            shutil.copytree(res_path, dist)
+        except OSError as err:
+            print('Для ' + rm_name + 'не создан шаблон!!!')
+            print(err)
+
+    def check_folder(self, path, count=0):
+        if os.path.isdir(path + str(count)):
+            self.check_folder(path, (count + 1))
+        else:
+            return path + str(count)
